1c0fadd6bb29af4b301ce512ba8a8e8f4bdf3588
Mohamad Fajar Oktafian <110889222+ajayback@users.noreply.github.com>
2025-01-31 15:42:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "9002178"))
+API_HASH = getenv("API_HASH", "a315441ae0890980db3be9d404bc3c59")
+BOT_TOKEN = getenv("BOT_TOKEN", "2029907704:AAGbD6_C8rRhV0D8Zfyun6pVtk6-H89XdDY")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "953267481").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ajay:ajaytia05@ajay.ddt9w.mongodb.net/?retryWrites=true&w=majority&appName=ajay")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
